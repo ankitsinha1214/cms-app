42ca7c9c38f9367c884ffcbab3155a61e8da112a
Ankit <Ankit.sinha@esyasoft.com>
2025-02-13 18:06:22 +0530

    updates

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAZX2pzPn-73NHcZV0ZKsa4gUkqm0LIS_U",
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
     authDomain: "chrgup-india.firebaseapp.com",
     projectId: "chrgup-india",
     storageBucket: "chrgup-india.firebasestorage.app",
